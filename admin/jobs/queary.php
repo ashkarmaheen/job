f67c50bfaad6166130273d6bbd76b0c4a5478415
unknown <ashkuashkar838@gmail.com>
2025-01-08 16:15:15 +0530

    addjob

--- admin/jobs/queary.php
+++ admin/jobs/queary.php
@@ -14,6 +14,9 @@ class jobs extends DBcon
     public $education;
     public $location;
     public $description;
+    public $img;
+    public $jobtable;
+    public $dlt;
 
 
 
@@ -48,7 +51,7 @@ class jobs extends DBcon
 
 
             $this->companyname = $_POST['companyname'];
-            $this->image = $_POST['image'];
+            $this->image = $_FILES['file'];
             $this->jobtype = $_POST['jobtype'];
             $this->industries = $_POST['industries'];
             $this->roles = $_POST['roles'];
@@ -57,10 +60,28 @@ class jobs extends DBcon
             $this->location = $_POST['location'];
             $this->description = $_POST['description'];
 
+            $img = "../../image/";
+
+            $ff = $img . basename($this->image["name"]);
+
+
+
+            if (pathinfo($ff, PATHINFO_EXTENSION) == "png" && $this->image["size"] > 500000) {
+
+                move_uploaded_file($this->image["tmp_name"], $ff);
+
+                echo "ok";
+            } else {
+
+                echo "error";
+            }
+
             try {
                 $req = "INSERT INTO jobs(companyname,image,jobtype,industries,roles,skills,education,location,description) 
-                value('$this->companyname','$this->image','$this->jobtype',' $this->industries',' $this->roles','$this->skills',' $this->education','$this->location','$this->description')";
+                value('$this->companyname','$ff','$this->jobtype',' $this->industries',' $this->roles','$this->skills',' $this->education','$this->location','$this->description')";
                 $this->con->exec($req);
+
+
 
                 header("location:index.php");
             } catch (PDOException $e) {
@@ -69,7 +90,48 @@ class jobs extends DBcon
         } else {
         }
     }
+
+
+
+    public function getjob()
+    {
+
+        try {
+            $this->jobtable = $this->con->prepare("SELECT * FROM  jobs ");
+            $this->jobtable->execute();
+
+            $result = $this->jobtable->setFetchMode(PDO::FETCH_ASSOC);
+            $tb = $this->jobtable->fetchAll();
+
+            return ($tb);
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+
+
+    public function dlt()
+    {
+        if (isset($_POST["delete"])) {
+
+
+            $this->dlt = $_POST['delete'];
+
+            try {
+                $sql = "DELETE FROM jobs WHERE id= $this->dlt";
+                $this->con->exec($sql);
+
+                header("location:index.php");
+            } catch (PDOException $e) {
+                echo $sql . "<br>" . $e->getMessage();
+            }
+        } else {
+        };
+    }
 }
 $jobsl = new jobs();
 $jobsl->jobfn();
 $jobsl->addjobfn();
+$jobsl->getjob();
+$jobsl->dlt();
